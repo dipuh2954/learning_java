a1b36fff984f5e3772b33d75dbafae0ffbdb4a4e
dipuhalder <dipuh2954@gmail.com>
2026-08-12 09:30:29 +0530

    still practicing

---
+++ src/sum_of_two_numbers.java
@@ -0,0 +1,6 @@
+public class sum_of_two_numbers {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 10;
+        System.out.println("Sum of two numbers is "+(num1+num2));
+    }
+}
