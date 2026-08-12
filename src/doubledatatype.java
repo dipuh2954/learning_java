b9de1e88e4a17f86f2be53fd264341cbf66d7970
dipuhalder <dipuh2954@gmail.com>
2026-08-12 10:31:57 +0530

    still practicing

---
+++ src/doubledatatype.java
@@ -0,0 +1,12 @@
+public class doubledatatype {
+    public static void main(String[] args) {
+        double x = 5;
+        double y = 2;
+        double z = x/y;
+        System.out.println(z);
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+    }
+}
