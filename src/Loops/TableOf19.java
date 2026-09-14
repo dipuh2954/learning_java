a5a93fb4c4c630ac9b8b40f2a1232dbe19781b03
dipuhalder <dipuh2954@gmail.com>
2026-09-14 05:39:16 +0530

    still practicing

--- src/Loops/TableOf19.java
+++ src/Loops/TableOf19.java
@@ -2,8 +2,8 @@ package Loops;
 
 public class TableOf19 {
     public static void main(String[] args) {
-        for (int i = 1; i<=10; i++){
-            System.out.println("19 * "+i+" = "+(19*i));
+        for (int i = 1; i<=10; i++) {
+            System.out.println("19 * " + i + " = " + (19 * i));
         }
     }
 }
---
+++ src/Loops/TableProgram.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class TableProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print it's table: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i<=10; i++){
+            System.out.println(num+" * "+i+" = "+(num*i));
+        }
+    }
+}
