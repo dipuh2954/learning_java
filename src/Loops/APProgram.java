8f1719571e53eda858802a1174d8c5f8582bec21
dipuhalder <dipuh2954@gmail.com>
2026-09-14 06:37:30 +0530

    still practicing

--- src/Loops/APProgram.java
+++ src/Loops/APProgram.java
@@ -8,7 +8,22 @@ public class APProgram {
         System.out.print("Enter the number upto you want to print: ");
         int num = sc.nextInt();
 
+        for (int i = 1; i<=(2*num-1); i+=2){
+            System.out.print(i+" ");
+        }
+        System.out.println();
         for (int i = 4; i<=(3*num+1); i+=3){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        System.out.print("Enter the starting number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the common difference: ");
+        int d = sc.nextInt();
+        int ap = a+(num-1)*d;
+
+        for (int i =a; i<=ap; i+=d){
             System.out.print(i+" ");
         }
     }
